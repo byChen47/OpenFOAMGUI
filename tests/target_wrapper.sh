b902979b344c56a07f078357481197b80ae7aeda
chenboyao-47 <1574530175@qq.com>
2026-05-12 18:47:05 +0800

    Clean: gitignore test artifacts

--- tests/target_wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-PATH=/D/3.Wpsandother/Qt/setting/6.10.2/mingw_64/bin:$PATH
-export PATH
-QT_PLUGIN_PATH=/D/3.Wpsandother/Qt/setting/6.10.2/mingw_64/plugins${QT_PLUGIN_PATH:+:$QT_PLUGIN_PATH}
-export QT_PLUGIN_PATH
-exec "$@"
